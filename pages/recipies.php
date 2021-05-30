67a5b7b2dcdda2917729ff54473a0cd3f4e43406
sttebaku <t.bakuradze@gmx.de>
2021-05-30 15:46:08 +0200

    structure test

--- pages/recipies.php
+++ pages/recipies.php
@@ -1,8 +1,10 @@
 <?php require_once '../config/dbConnect.php';
 include_once '../includes/header.php'; ?>
-<div class="container"></div>
-<?php include_once '../includes/footer.php';
+<div class="container">
+<?php
 $conn = OpenCon();
 echo "Connected Successfully";
 CloseCon($conn);
 ?>
+</div>
+<?php include_once '../includes/footer.php'; ?>
